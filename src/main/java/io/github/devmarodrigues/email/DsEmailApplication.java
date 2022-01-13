e8301a03e9ad8d7790bca2cb014e8fcde372601a
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-01-13 16:44:35 -0300

    Projeto criado

---
+++ src/main/java/io/github/devmarodrigues/email/DsEmailApplication.java
@@ -0,0 +1,13 @@
+package io.github.devmarodrigues.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devmarodrigues/email/DsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devmarodrigues.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
